a65363e022771a6507d72d24bf71d9b6d2fe88c2
huseyinkayayurt <huseyinkayayurt@gmail.com>
2021-04-17 17:54:49 +0300

    Express Generator REST API

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -7,7 +7,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
 var app = express();
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +31,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
